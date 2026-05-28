9e8fad027dfb98bb42f425774a353ce2d40e0b32
SagarRathode <32382376+SagarRathode@users.noreply.github.com>
2026-05-28 14:51:02 +0530

    added power feature

--- CalculatorModular/Parser.cpp
+++ CalculatorModular/Parser.cpp
@@ -1,6 +1,7 @@
 #include "Parser.hpp"
 
 #include <stdexcept>
+#include <cmath>
 
 Parser::Parser(Tokenizer tokenizer)
     : m_tokenizer(tokenizer),
@@ -34,6 +35,113 @@ double Parser::parse()
 
 double Parser::parseExpression()
 {
+    double result = parseTerm();
+
+    while (
+        m_currentToken.type == TokenType::Plus ||
+        m_currentToken.type == TokenType::Minus)
+    {
+        TokenType op = m_currentToken.type;
+
+        eat(op);
+
+        if (op == TokenType::Plus)
+        {
+            result += parseTerm();
+        }
+        else
+        {
+            result -= parseTerm();
+        }
+    }
+
+    return result;
+}
+
+double Parser::parseTerm()
+{
+    double result = parsePower();
+
+    while (
+        m_currentToken.type == TokenType::Multiply ||
+        m_currentToken.type == TokenType::Divide ||
+        m_currentToken.type == TokenType::Module )
+    {
+        TokenType op = m_currentToken.type;
+
+        eat(op);
+
+        double rhs = parsePower();
+
+        if (op == TokenType::Multiply)
+        {
+            result *= rhs;
+        }
+        else if(op == TokenType::Module)
+        {
+            if(rhs == 0.0)
+            {
+                throw std::runtime_error("module by zero");
+            }
+
+            result = std::fmod(result, rhs);
+        }
+        else
+        {
+            if (rhs == 0.0)
+            {
+                throw std::runtime_error("Division by zero");
+            }
+
+            result /= rhs;
+        }
+    }
+
+    return result;
+}
+
+double Parser::parsePower()
+{
+    double result = parseFactor();
+
+    if (m_currentToken.type == TokenType::Power)
+    {
+        eat(TokenType::Power);
+
+        double rhs = parsePower();
+
+        result = std::pow(result, rhs);
+    }
+
+    return result;
+}
+
+double Parser::parseFactor()
+{
+    if (m_currentToken.type == TokenType::Number)
+    {
+        double value = m_currentToken.value;
+
+        eat(TokenType::Number);
+
+        return value;
+    }
+
+    if (m_currentToken.type == TokenType::LeftParen)
+    {
+        eat(TokenType::LeftParen);
+
+        double result = parseExpression();
+
+        eat(TokenType::RightParen);
+
+        return result;
+    }
+
+
+    throw std::runtime_error("Expected number or '('");
+}
+
     double result = parseTerm();
 
     while (
